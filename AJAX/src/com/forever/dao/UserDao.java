013af39c55d4f7e67afca2bc827b3f92e472d99b
MrBlueiris <46697589+MrBlueiris@users.noreply.github.com>
2019-07-31 10:00:11 +0800

    delete

--- AJAX/src/com/forever/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.forever.dao;
-
-import java.util.List;
-
-import com.forever.pojo.User;
-
-public interface UserDao {
-	public List<User> getAllUser();
-}
--- AJAX/src/com/forever/service/IUserService.java
+++
@@ -1,9 +0,0 @@
-package com.forever.service;
-
-import java.util.List;
-
-import com.forever.pojo.User;
-
-public interface IUserService {
-	public List<User> getAllUser();
-}
